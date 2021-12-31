c27605cc0c03a03778dec257e0b040a4d11a288d
Jin-devST <kpasd002@gmail.com>
2021-12-31 15:17:26 +0900

    이진 검색알고리즘 재귀함수 풀이

--- search/Tree Search/binarytree_search_1.py
+++ search/Tree Search/binarytree_search_1.py
@@ -1,1 +1,29 @@
 # 부품 찾기
+# 부품 n 개가 있다 각 부품은 정수 형태의 고유한 번호가 있다.
+# 손님이 m개 종류의 부품을 대량으로 구매하겠다며 당일날 견적서를 요청했다. 동빈이는 때를 놓치지 않고 손님이 문의한 부품 m개 종류를 모두 확인하여 견적서를 작성행햐ㅏㄴ다. 이때 가게 안에 부품이 모두 있는지 확인하는 프로그램을 작성해보자
+def binary_search(array, start, end, target):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            # result.append(array_n[mid])
+            return mid
+        elif array[mid] < target:
+            binary_search(mid + 1, end, target)
+        else:
+            binary_search(start, mid - 1, target)
+    return None
+
+
+n = int(input())
+array_n = list(map(int, input().split()))  # 전자매장 부품
+m = int(input())
+array_m = list(map(int, input().split()))  # 필요한 부품
+
+array_n.sort()
+
+for x in array_m:
+    result = binary_search(array_n, 0, n - 1, x)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
