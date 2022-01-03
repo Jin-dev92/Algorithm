a3e3b856c5a05bc4836ca614afc9a73bce591651
Jin-devST <kpasd002@gmail.com>
2022-01-04 00:18:03 +0900

    동적 프로그래밍 문제 추가

--- dynamic programing/question/question2.py
+++ dynamic programing/question/question2.py
@@ -1,5 +1,20 @@
 import time
-# 개미 전사
-start = time.time()
 
+# 개미전사
+start = time.time()
+n = int(input())
+array = list(map(int, input().split()))
+# d = [0] * 100
+# d[0] = 0
+# d[1] = array[0]
+# for i in range(2, n):
+#     d[i] = array[i - 2] + array[i]
+# print(max(d))
+
+d = [0] * 100
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + array[i])
+print(d[n - 1])
 print(time.time() - start)
